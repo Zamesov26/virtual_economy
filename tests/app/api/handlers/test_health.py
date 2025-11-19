8c7777a8d7066ebe95664f26cb361f4e94ded8c8
Oleg Zamesov <o.zamesov@yandex.ru>
2025-11-19 17:35:56 +0300

    fix gitignore

---
+++ tests/app/api/handlers/test_health.py
@@ -0,0 +1,12 @@
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.anyio
+class TestHealth:
+    async def test_should_return_healthy_status_when_getting_health_endpoint(self, client: AsyncClient):
+        response = await client.get("/api/v1/health")
+        assert response.status_code == 200
+
+        data = response.json()
+        assert data == {"status": "healthy", "message": "FastAPI service is running"}
